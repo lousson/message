e4aa9a0a0b203e24fc092170c6e24a696624c838
Matze <mhennig@quirkies.org>
2013-06-11 16:24:42 +0200

    Introduced the AnyMessageFactory interface

---
+++ src/php/Lousson/Message/AnyMessageFactory.php
@@ -0,0 +1,74 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 textwidth=75: *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (c) 2013, The Lousson Project                               *
+ *                                                                       *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * Redistribution and use in source and binary forms, with or without    *
+ * modification, are permitted provided that the following conditions    *
+ * are met:                                                              *
+ *                                                                       *
+ * 1) Redistributions of source code must retain the above copyright     *
+ *    notice, this list of conditions and the following disclaimer.      *
+ * 2) Redistributions in binary form must reproduce the above copyright  *
+ *    notice, this list of conditions and the following disclaimer in    *
+ *    the documentation and/or other materials provided with the         *
+ *    distribution.                                                      *
+ *                                                                       *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   *
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     *
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     *
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE        *
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,            *
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    *
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR    *
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)    *
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,   *
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)         *
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED   *
+ * OF THE POSSIBILITY OF SUCH DAMAGE.                                    *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**
+ *  Lousson\Message\AnyMessageFactory interface definition
+ *
+ *  @package    org.lousson.message
+ *  @copyright  (c) 2013, The Lousson Project
+ *  @license    http://opensource.org/licenses/bsd-license.php New BSD License
+ *  @author     Mathias J. Hennig <mhennig at quirkies.org>
+ *  @filesource
+ */
+namespace Lousson\Message;
+
+/**
+ *  An interface for message factories
+ *
+ *  The AnyMessageFactory interface declares the API to be provided by
+ *  any message factory.
+ *
+ *  @since      lousson/Lousson_Message-0.1.0
+ *  @package    org.lousson.message
+ */
+interface AnyMessageFactory
+{
+    /**
+     *  Create a message instance
+     *
+     *  The getMessage() method is used to create a message instance to
+     *  represent the provided $content. The optional $type parameter can
+     *  be used to specify the (internet-) media-type of the content.
+     *
+     *  @param  mixed               $content    The message data
+     *  @param  string              $type       The message media type
+     *
+     *  @return \Lousson\Message\AnyMessage
+     *          A message instance is returned on success
+     *
+     *  @throws \Lousson\Message\AnyMessageException
+     *          Raised in case the message could not get created
+     */
+    public function getMessage($data, $type = null);
+}
+
